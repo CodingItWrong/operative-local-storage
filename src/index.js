20309d326f73a51012cf2a33ee8ff3cbd66909c1
Josh Justice <josh.justice@accesscfa.com>
2019-12-25 19:53:25 -0500

    Add code

---
+++ src/index.js
@@ -0,0 +1,28 @@
+export default class LocalStoragePersister {
+  #key
+
+  constructor(key) {
+    if (!key) throw new Error('key must be provided')
+
+    this.#key = key
+  }
+
+  save(data) {
+    const serializedData = JSON.stringify(data)
+    localStorage.setItem(this.#key, serializedData)
+    console.log('LocalStoragePersister.save()', serializedData)
+    return Promise.resolve()
+  }
+
+  load() {
+    const serializedData = localStorage.getItem(this.#key)
+    console.log('LocalStoragePersister.load()', serializedData)
+    let data
+    if (serializedData == null) {
+      data = null
+    } else {
+      data = JSON.parse(serializedData)
+    }
+    return Promise.resolve(data)
+  }
+}
